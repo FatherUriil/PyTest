721615e57d659eacd7c1b109ee41601c6b076f5a
FatherUriil <reraEfimova28@gmail.com>
2024-05-30 10:56:58 +0300

    Add files via upload

---
+++ praktika3.py
@@ -0,0 +1,47 @@
+# Задание 1
+
+print("Задание 1")
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл"]
+print(f"Last: {list_players[-1]}\nFirst: {list_players[0]}")
+
+# Задание 2
+
+print()
+print("Задание 2")
+shopping_list = ["яблоко", "молоко", "хлеб", "яблоко", "масло", "яйца", "молоко"]
+print(len(set(shopping_list)))
+
+# Задание 3
+
+print()
+print("Задание 3")
+print({1: "Winter", 2: "Spring", 3: "Summer", 4: "Autumn"})
+
+# Задание 4
+
+print()
+print("Задание 4")
+users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
+print(f"Users count: {len(users)}\nUniquer users count: {len(set(users))}")
+
+# Задание 5
+
+print()
+print("Задание 5")
+list_participants = ["Орлов", "Петров", "Иванов", "Сидоров", "Васильев", "Черепахин"]
+print(f"Last: {list_participants[-1]}\nFirst: {list_participants[0]}")
+
+# Задание 6
+
+print()
+print("Задание 6")
+sps = ["Дубровский", "Горе от ума", "Кавказский пленник", "Хамелеон", "Ревизор", "Гранатовый браслет"]
+print(f"Last: {sps[-1]}\nFirst: {sps[0]}")
+
+# Задание 7
+
+print()
+print("Задание 7")
+shopping_list = ["Палатка", "Спальник", "Котелок", "Тренога", "Рюкзак", "Спальник", "Рюкзак", "Термос", "Миска",
+                 "Ветровка", "Коврик"]
+print(f"Count: {len(set(shopping_list))}")
